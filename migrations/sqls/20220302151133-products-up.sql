04b9a21b670c9ffd4fbfbc60c17b609f1c08e6be
reinhold <reinhold.beny@ad-alliance.de>
2022-10-18 13:10:10 +0200

    update migrations for products with url and decription

--- migrations/sqls/20220302151133-products-up.sql
+++ migrations/sqls/20220302151133-products-up.sql
@@ -3,6 +3,8 @@ CREATE TABLE products(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     price INTEGER,
-    category VARCHAR(80)
+    category VARCHAR(80),
+    url VARCHAR(1000),
+    description VARCHAR(80),
 )
 
